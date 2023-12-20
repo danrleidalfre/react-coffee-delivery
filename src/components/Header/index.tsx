0630e0b48cc5300f9e2c8931071bc12b9d1d9a51
Danrlei <danrleidalfre@gmail.com>
2023-12-20 15:12:46 -0300

    implement header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,27 @@
-import { HeaderContainer } from './styles'
+import { HeaderContainer, HeaderContent, Nav } from './styles'
+
+import logo from '../../assets/logo.svg'
+import { NavLink } from 'react-router-dom'
+import { MapPin, ShoppingCart } from 'phosphor-react'
 
 export function Header() {
   return (
     <HeaderContainer>
-      <h1>Header</h1>
+      <HeaderContent>
+        <NavLink to="/">
+          <img src={logo} alt="" />
+        </NavLink>
+        <Nav>
+          <div>
+            <MapPin size={22} weight={'fill'} />
+            <span>Florianópolis, SC</span>
+          </div>
+          <NavLink to="/checkout">
+            <ShoppingCart size={22} weight={'fill'} />
+            <span>3</span>
+          </NavLink>
+        </Nav>
+      </HeaderContent>
     </HeaderContainer>
   )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,3 +1,72 @@
 import styled from 'styled-components'
 
-export const HeaderContainer = styled.header``
+export const HeaderContainer = styled.header`
+  position: fixed;
+  display: flex;
+  width: 100%;
+  padding: 0 1rem;
+  background-color: ${(props) => props.theme.background};
+`
+
+export const HeaderContent = styled.div`
+  max-width: 70rem;
+  width: 100%;
+  margin: 0 auto;
+  padding: 2.0625rem 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+export const Nav = styled.nav`
+  display: flex;
+  align-items: center;
+
+  div {
+    padding: 0 0.5rem;
+    background-color: ${(props) => props.theme['purple-light']};
+    color: ${(props) => props.theme['purple-dark']};
+    border-radius: 6px;
+    font-size: 0.875rem;
+    display: flex;
+    align-items: center;
+    height: 2.375rem;
+
+    svg {
+      color: ${(props) => props.theme.purple};
+    }
+
+    span {
+      margin-left: 0.4rem;
+    }
+  }
+
+  a {
+    background-color: ${(props) => props.theme['yellow-light']};
+    color: ${(props) => props.theme['yellow-dark']};
+    width: 2.375rem;
+    height: 2.375rem;
+    border-radius: 6px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-left: 0.75rem;
+    position: relative;
+
+    span {
+      position: absolute;
+      top: -7px;
+      right: -7px;
+      background-color: ${(props) => props.theme['yellow-dark']};
+      width: 1.25rem;
+      height: 1.25rem;
+      border-radius: 50%;
+      color: ${(props) => props.theme.white};
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-weight: bold;
+      font-size: 0.75rem;
+    }
+  }
+`
