2068723a8e9f643001a6ab78c3620dde9379449f
Danrlei <danrleidalfre@gmail.com>
2023-12-28 19:20:13 -0300

    implement banner

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,63 @@
-import { HomeContainer } from './styles'
+import {
+  Banner,
+  Coffees,
+  HomeContainer,
+  IconRoundedCart,
+  IconRoundedCoffee,
+  IconRoundedPackage,
+  IconRoundedTimer,
+  Itens,
+} from './styles'
+
+import image from '../../assets/banner.png'
+import { Coffee, Package, ShoppingCart, Timer } from 'phosphor-react'
 
 export function Home() {
   return (
     <HomeContainer>
-      <h1></h1>
+      <Banner>
+        <div>
+          <h1>
+            Encontre o café perfeito
+            <br />
+            para qualquer hora do dia
+          </h1>
+          <p>
+            Com o Coffee Delivery você recebe seu café onde estiver, a <br />
+            qualquer hora
+          </p>
+          <Itens>
+            <h3>
+              <IconRoundedCart>
+                <ShoppingCart weight={'fill'} size={18} />
+              </IconRoundedCart>
+              Compra simples e segura
+            </h3>
+            <h3>
+              <IconRoundedPackage>
+                <Package weight={'fill'} size={18} />
+              </IconRoundedPackage>
+              Embalagem mantém o café intacto
+            </h3>
+            <h3>
+              <IconRoundedTimer>
+                <Timer weight={'fill'} size={18} />
+              </IconRoundedTimer>
+              Entrega rápida e rastreada
+            </h3>
+            <h3>
+              <IconRoundedCoffee>
+                <Coffee weight={'fill'} size={18} />
+              </IconRoundedCoffee>
+              O café chega fresquinho até você
+            </h3>
+          </Itens>
+        </div>
+        <img src={image} alt="" />
+      </Banner>
+      <Coffees>
+        <h2>Nossos Cafés</h2>
+      </Coffees>
     </HomeContainer>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,92 @@
 import styled from 'styled-components'
 
-export const HomeContainer = styled.main``
+export const HomeContainer = styled.main`
+  padding-top: 6.875rem;
+  background-image: url('/src/assets/bg.png');
+  background-repeat: no-repeat;
+  background-size: 100% 65%;
+  background-position: center;
+`
+
+export const Banner = styled.section`
+  max-width: 70rem;
+  padding: 6.25rem 0;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  h1 {
+    font-size: 3rem;
+    font-family: 'Baloo 2', sans-serif;
+    font-weight: 900;
+    color: ${(props) => props.theme['base-title']};
+    line-height: 130%;
+    margin-bottom: 1rem;
+  }
+
+  p {
+    font-size: 1.25rem;
+    color: ${(props) => props.theme['base-subtitle']};
+    line-height: 130%;
+    margin-bottom: 4.125rem;
+  }
+`
+
+export const Itens = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 1.25rem;
+
+  h3 {
+    display: flex;
+    align-items: center;
+    font-size: 1rem;
+    font-weight: 400;
+  }
+`
+
+export const IconRounded = styled.div`
+  width: 2rem;
+  height: 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 1rem;
+  margin-right: 0.75rem;
+
+  svg {
+    color: ${(props) => props.theme.white};
+  }
+`
+
+export const IconRoundedCart = styled(IconRounded)`
+  background-color: ${(props) => props.theme['yellow-dark']};
+`
+
+export const IconRoundedPackage = styled(IconRounded)`
+  background-color: ${(props) => props.theme['base-text']};
+`
+
+export const IconRoundedTimer = styled(IconRounded)`
+  background-color: ${(props) => props.theme.yellow};
+`
+
+export const IconRoundedCoffee = styled(IconRounded)`
+  background-color: ${(props) => props.theme.purple};
+`
+
+export const Coffees = styled.section`
+  margin: 2rem auto 0;
+  max-width: 70rem;
+
+  h2 {
+    font-size: 2rem;
+    font-family: 'Baloo 2', sans-serif;
+    font-weight: 900;
+    color: ${(props) => props.theme['base-title']};
+    line-height: 130%;
+  }
+`
+
+export const Coffee = styled.section``
