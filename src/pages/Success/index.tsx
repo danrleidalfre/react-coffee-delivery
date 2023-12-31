5b82a7d2ae486beec490a4a6c5787fe1d9583b7c
Danrlei <danrleidalfre@gmail.com>
2023-12-31 11:43:15 -0300

    implement success order page

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,9 +1,56 @@
-import { SuccessContainer } from './styles'
+import {
+  Address,
+  Delivery,
+  Forecast,
+  H1,
+  H2,
+  Icon,
+  Infos,
+  Payment,
+  SuccessContainer,
+} from './styles'
+import { CurrencyDollar, MapPin, Timer } from 'phosphor-react'
 
 export function Success() {
   return (
     <SuccessContainer>
-      <h1>Success</h1>
+      <H1>Uhu! Pedido confirmado</H1>
+      <H2>Agora é só aguardar que logo o café chegará até você</H2>
+      <Delivery>
+        <Infos>
+          <Address>
+            <Icon>
+              <MapPin size={16} weight={'fill'} />
+            </Icon>
+            <h3>
+              Entrega em <span>Travessa Lua Nova, 121</span>
+              <br />
+              Ingleses - Florianópolis, SC
+            </h3>
+          </Address>
+          <Forecast>
+            <Icon>
+              <Timer size={16} weight={'fill'} />
+            </Icon>
+            <h3>
+              Previsão de entrega
+              <br />
+              <span>20 min - 30 min</span>
+            </h3>
+          </Forecast>
+          <Payment>
+            <Icon>
+              <CurrencyDollar size={16} weight={'fill'} />
+            </Icon>
+            <h3>
+              Pagamento na entrega
+              <br />
+              <span>Cartão de Crédito</span>
+            </h3>
+          </Payment>
+        </Infos>
+        <img src="/src/assets/delivery.svg" alt="" />
+      </Delivery>
     </SuccessContainer>
   )
 }
--- src/pages/Success/styles.ts
+++ src/pages/Success/styles.ts
@@ -1,3 +1,101 @@
 import styled from 'styled-components'
 
-export const SuccessContainer = styled.main``
+export const SuccessContainer = styled.main`
+  padding-top: 11.5rem;
+  max-width: 70rem;
+  margin: 0 auto;
+`
+
+export const H1 = styled.h1`
+  font-size: 2rem;
+  font-weight: 900;
+  color: ${(props) => props.theme['yellow-dark']};
+  font-family: 'Baloo 2', sans-serif;
+  line-height: 130%;
+`
+
+export const H2 = styled.h2`
+  margin-top: 4px;
+  font-size: 1.25rem;
+  color: ${(props) => props.theme['base-subtitle']};
+  font-weight: normal;
+  line-height: 130%;
+`
+
+export const Delivery = styled.section`
+  margin-top: 2.5rem;
+  display: flex;
+  justify-content: space-between;
+`
+
+export const Infos = styled.div`
+  padding: 2.5rem;
+  max-width: 32.875rem;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  border-radius: 6px 36px;
+  position: relative;
+
+  &::before {
+    content: '';
+    position: absolute;
+    inset: 0;
+    border-radius: 6px 36px;
+    padding: 1px;
+    background: linear-gradient(
+      135deg,
+      ${(props) => props.theme.yellow},
+      ${(props) => props.theme.purple}
+    );
+    -webkit-mask:
+      linear-gradient(${(props) => props.theme.background} 0 0) content-box,
+      linear-gradient(${(props) => props.theme.background} 0 0);
+    -webkit-mask-composite: xor;
+    mask-composite: exclude;
+  }
+`
+
+export const Info = styled.div`
+  display: flex;
+  align-items: center;
+
+  h3 {
+    font-size: 1rem;
+    font-weight: normal;
+
+    span {
+      font-weight: bold;
+    }
+  }
+`
+
+export const Address = styled(Info)`
+  div {
+    background-color: ${(props) => props.theme.purple};
+  }
+`
+
+export const Forecast = styled(Info)`
+  div {
+    background-color: ${(props) => props.theme.yellow};
+  }
+`
+
+export const Payment = styled(Info)`
+  div {
+    background-color: ${(props) => props.theme['yellow-dark']};
+  }
+`
+
+export const Icon = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 2rem;
+  height: 2rem;
+  border-radius: 1rem;
+  margin-right: 0.75rem;
+  color: ${(props) => props.theme.background};
+`
